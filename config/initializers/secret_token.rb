6836b50528c1a8145e6154123bcf7f84ba075bb3
timstone <tim.stone@yahoo.co.uk>
2016-07-20 11:22:45 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Browserslack::Application.config.secret_key_base = '045c8c9aee617d2f4427cefa99fdd8da76672cfa013ab066a62f621daac72df5467261b38f63737b9c3658ae983fa8f88a3905c3a1d66a3f87ed02e781d6b66b'
